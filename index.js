36c60e0e3690bf1785a7e2940f171a65d8490cef
unknown <leoczuy@gmail.com>
2022-12-02 18:45:37 -0300

    v0.1

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js")
+const config = require("./config.json")
+
+const client = new Discord.Client({ intents: [ Discord.GatewayIntentBits.Guilds]});
+
+module.exports = client
+
+client.on('interactionCreate', (interaction) => {
+
+  if(interaction.type === Discord.InteractionType.ApplicationCommand){
+
+      const cmd = client.slashCommands.get(interaction.commandName);
+
+      if (!cmd) return interaction.reply(`Error`);
+
+      interaction["member"] = interaction.guild.members.cache.get(interaction.user.id);
+
+      cmd.run(client, interaction)
+   }
+})
+
+client.on('ready', () => {
+  console.log(`Estou online!`)
+})
+
+client.slashCommands = new Discord.Collection()
+
+require('./handler')(client)
+
+client.login(config.token)
+
